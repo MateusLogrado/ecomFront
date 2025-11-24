cc0730441d86ebee7af430632ac015eeb996d81c
Mateus Queiroz <mateus_logrado@estudante.sesisenai.org.br>
2025-11-24 09:44:28 -0300

    primeiro commit front

---
+++ js/usuario/cadastro.js
@@ -0,0 +1,35 @@
+let res = document.getElementById("res")
+let button = document.getElementById("button")
+
+button.addEventListener("click", (e)=>{
+    e.preventDefault()
+
+    let nome = document.getElementById("nome").value
+    let email = document.getElementById("email").value
+    let senha = document.getElementById("senha").value
+    let telefone = document.getElementById("telefone").value
+    let cpf = document.getElementById("cpf").value
+    let identidade = document.getElementById("identidade").value
+
+    const valores = {
+        nome: nome,
+        email: email,
+        senha: senha,
+        telefone: telefone,
+        cpf: cpf,
+        identidade: identidade
+    }
+
+    fetch(`http://localhost:3000/usuario`, {
+        method: "POST",
+        headers: { "content-type":"application/json" },
+        body: JSON.stringify(valores)
+    })
+    .then(resp => resp.json())
+    .then(dados =>{
+        res.innerHTML = dados.message   
+    })
+    .catch(err =>{
+        console.error("Erro ao cadastrar o usuario: ", err)
+    })
+})
