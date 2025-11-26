28be0bc10594782c16b0380eb9e26e7bf118630e
Mateus Queiroz <mateus_logrado@estudante.sesisenai.org.br>
2025-11-26 12:44:02 -0300

    usuario quase completo

--- js/nav.js
+++ js/nav.js
@@ -4,5 +4,5 @@ const token = sessionStorage.getItem("token")
 if(!token){
     nav.innerHTML = `<a href="./html/usuario/cadastrar.html">Cadastrar</a> <a href="./html/usuario/login.html">Login</a>`
 }else{
-    
+    nav.innerHTML = `<a href="./html/usuario/config.html">Configuração</a>`
 }
